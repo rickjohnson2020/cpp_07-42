34569962a8e0d49cffa64937781d49bb2b56140e
Riku Yano <riyano@student.42london.com>
2026-03-20 18:17:11 +0000

    ready to submit

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -1,6 +1,8 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <iostream>
+
 template <typename T>
 class Array {
 private:
@@ -8,14 +10,52 @@ private:
 	unsigned int _size;
 
 public:
-	Array() : array(NULL), _size(0) {}
+	Array() : data(NULL), _size(0) {
+		std::cout << "Default constructor called" << std::endl;
+	}
 	
 	Array(unsigned int n) : _size(n) {
-		data = new T[n](); // if put (), int will be initialized by 0
+		std::cout << "Size constructor called" << std::endl;
+		data = new T[n]();
 	}
 
-	Array(const Array& other) {
-		
+	Array(const Array& other) : _size(other._size) {
+		std::cout << "Copy constructor called" << std::endl;
+		data = new T[_size];
+		for (unsigned int i = 0; i < _size; i++) {
+			data[i] = other.data[i];
+		}
+	}
+
+	~Array() {
+		std::cout << "Destructor called" << std::endl;
+		delete[] data;
+	}
+
+	// Array<int> a(3);
+	// a[0] = 1;
+	// Array<int> b = a;
+	// b[0] = 100
+	Array& operator=(const Array& other) {
+		std::cout << "Copy assignment operator called" << std::endl;
+		if (this != &other) {
+			delete[] data;
+			data = new T[_size];
+			for (unsigned int i = 0; i < _size; i++) {
+				data[i] = other.data[i];
+			}
+		}
+		return *this;
+	}
+
+	T& operator[](unsigned int index) {
+		if (index >= _size)
+			throw std::out_of_range("Index out of range");
+		return data[index];
+	}
+
+	unsigned int size() const {
+		return _size;
 	}
 };
 
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,8 +3,53 @@
 
 
 int main() {
-	Array<std::string> arr(3);
-	for (int i = 0; i < 3; i++) {
-		std::cout << arr[i] << std::endl;
+
+	std::cout << "===== TEST 1 =====" << std::endl;
+
+	Array<std::string> strArr1(4);
+	std::cout << "strArr1 size: " << strArr1.size() << std::endl;
+
+	try {
+		strArr1[0] = "hello";
+		strArr1[1] = "42";
+		strArr1[2] = "!";
+		strArr1[4] = "!";
+	} catch (const std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	Array<std::string> strArr2 = strArr1;
+	std::cout << "strArr2 size: " << strArr1.size() << std::endl;
+
+	for (int i = 0; i < 4; i++) {
+		try {
+			std::cout << strArr2[i] << std::endl;
+		} catch (const std::exception& e) {
+			std::cout << e.what() << std::endl;
+		}
+	}
+
+	std::cout << "===== TEST 2 =====" << std::endl;
+
+	Array<int> intArr1(10);
+	std::cout << "intArr1 size: " << intArr1.size() << std::endl;
+
+	for (int i = 4; i < 10; i++) {
+		try {
+			intArr1[i] = i;
+		} catch (const std::exception& e) {
+			std::cout << e.what() << std::endl;
+		}
+	}
+
+	Array<int> intArr2;
+	intArr2 = intArr1;
+
+	for (int i = 0; i < 11; i++) {
+		try {
+			std::cout << intArr2[i] << std::endl;
+		} catch (const std::exception& e) {
+			std::cout << e.what() << std::endl;
+		}
 	}
 }
