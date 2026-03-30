72febb0b64e41a42fccf658a2d5ccc995f409a35
yano <eriksontokyo2020@gmail.com>
2026-03-30 15:46:14 +0100

    refactor Array class: improve member variable naming and add exception handling in main

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,52 +6,55 @@
 template <typename T>
 class Array {
 private:
-	T* data;
+	T* _data;
 	unsigned int _size;
 
 public:
-	Array() : data(NULL), _size(0) {
+	Array() : _data(NULL), _size(0) {
 		std::cout << "Default constructor called" << std::endl;
 	}
 	
 	Array(unsigned int n) : _size(n) {
 		std::cout << "Size constructor called" << std::endl;
-		data = new T[n]();
+		_data = new T[n]();
 	}
 
 	Array(const Array& other) : _size(other._size) {
 		std::cout << "Copy constructor called" << std::endl;
-		data = new T[_size];
+		_data = new T[_size];
 		for (unsigned int i = 0; i < _size; i++) {
-			data[i] = other.data[i];
+			_data[i] = other._data[i];
 		}
 	}
 
 	~Array() {
 		std::cout << "Destructor called" << std::endl;
-		delete[] data;
+		delete[] _data;
 	}
 
-	// Array<int> a(3);
-	// a[0] = 1;
-	// Array<int> b = a;
-	// b[0] = 100
 	Array& operator=(const Array& other) {
 		std::cout << "Copy assignment operator called" << std::endl;
 		if (this != &other) {
-			delete[] data;
-			data = new T[_size];
+			_size = other._size;
+			delete[] _data;
+			_data = new T[_size];
 			for (unsigned int i = 0; i < _size; i++) {
-				data[i] = other.data[i];
+				_data[i] = other._data[i];
 			}
 		}
 		return *this;
 	}
 
+	const T& operator[](unsigned int index) const {
+		if (index >= _size)
+			throw std::out_of_range("Index out of range");
+		return _data[index];
+	}
+
 	T& operator[](unsigned int index) {
 		if (index >= _size)
 			throw std::out_of_range("Index out of range");
-		return data[index];
+		return _data[index];
 	}
 
 	unsigned int size() const {
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,4 +1,5 @@
 #include "./Array.hpp"
+#include <exception>
 #include <iostream>
 
 
@@ -6,22 +7,36 @@ int main() {
 
 	std::cout << "===== TEST 1 =====" << std::endl;
 
-	Array<std::string> strArr1(4);
+	Array<std::string> strArr1(5);
 	std::cout << "strArr1 size: " << strArr1.size() << std::endl;
 
 	try {
-		strArr1[0] = "hello";
-		strArr1[1] = "42";
-		strArr1[2] = "!";
+		strArr1[0] = "42";
+		strArr1[1] = "is";
+		strArr1[2] = "over";
 		strArr1[4] = "!";
 	} catch (const std::exception& e) {
 		std::cout << e.what() << std::endl;
 	}
 
+	for (int i = 0; i < 5; i++) {
+		try {
+			std::cout << strArr1[i] << std::endl;
+		} catch (const std::exception& e) {
+			std::cout << e.what() << std::endl;
+		}
+	}
+
 	Array<std::string> strArr2 = strArr1;
 	std::cout << "strArr2 size: " << strArr1.size() << std::endl;
 
-	for (int i = 0; i < 4; i++) {
+	try {
+		strArr2[2] = "not over";
+	} catch (const std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	for (int i = 0; i < 5; i++) {
 		try {
 			std::cout << strArr2[i] << std::endl;
 		} catch (const std::exception& e) {
@@ -43,9 +58,18 @@ int main() {
 	}
 
 	Array<int> intArr2;
+	std::cout << "intArr2 size (before copy): " << intArr2.size() << std::endl;
+	try {
+		std::cout << intArr2[0] << std::endl;
+	} catch (const std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+	
 	intArr2 = intArr1;
+	intArr2[0] = 100;
+	std::cout << "intArr2 size (after copy): " << intArr2.size() << std::endl;
 
-	for (int i = 0; i < 11; i++) {
+	for (int i = 0; i < 10; i++) {
 		try {
 			std::cout << intArr2[i] << std::endl;
 		} catch (const std::exception& e) {
